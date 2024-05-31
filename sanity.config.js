b12be78c4824e2d08a43d4d51de5b30e6d0d2f20
stephanietako <gabeshine@live.fr>
2024-05-31 14:09:05 +0200

    Starting project part4-refacto-sanity update

--- sanity.config.js
+++ sanity.config.js
@@ -1,26 +1,21 @@
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.jsx` route
  */
-
-import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-
+import { structureTool } from "sanity/structure";
+import { schema } from "./schema";
+import { schemaTypes } from "./schema";
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schema";
 
 export default defineConfig({
   title: "projet-vibes",
   basePath: "/studio",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
-  plugins: [
-    deskTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
-  ],
+  apiVersion,
+  plugins: [structureTool()],
+  schema: {
+    types: schemaTypes,
+  },
 });
---
+++ schema.js
@@ -0,0 +1,27 @@
+import project from "./sanity/schemas/project-schema";
+import blockContent from "./sanity/schemas/blockContent";
+import page from "./sanity/schemas/page-schema";
+import product from "./sanity/schemas/product-schema";
+import category from "./sanity/schemas/category-schema";
+import starProduct from "./sanity/schemas/starProduct-schema";
+import plus from "./sanity/schemas/plus-schema";
+import user from "./sanity/schemas/user-schema";
+import account from "./sanity/schemas/account-schema";
+import verificationToken from "./sanity/schemas/verificationToken-schema";
+import plusProduct from "./sanity/schemas/plusProduct-schema";
+import star from "./sanity/schemas/star-schema";
+
+export const schemaTypes = [
+  project,
+  blockContent,
+  page,
+  product,
+  category,
+  star,
+  starProduct,
+  plus,
+  plusProduct,
+  user,
+  account,
+  verificationToken,
+];
